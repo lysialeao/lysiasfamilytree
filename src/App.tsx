39fef4136994cf6e9258ad09d38a8ae6d5d9747d
Lysia Leão <lysialeao@gmail.com>
2026-09-12 12:09:20 -0300

    feat: :sparkles: add simple landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react'
+
 import { FamilyTree } from './components/FamilyTree/FamilyTree'
+import { Landing } from './pages/Landing/Landing'
 
 function App() {
-  return <FamilyTree />
+  const [showTree, setShowTree] = useState(false)
+
+  if (showTree) {
+    return <FamilyTree />
+  }
+
+  return <Landing onStart={() => setShowTree(true)} />
 }
 
 export default App
